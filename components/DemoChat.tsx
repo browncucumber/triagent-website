f951dacd669196efe38da94323c27fe6537e0029
Dmitri <dmitri.parpiev@gmail.com>
2026-05-29 14:16:43 -0400

    fix webhook URL path in DemoChat

--- components/DemoChat.tsx
+++ components/DemoChat.tsx
@@ -47,7 +47,7 @@ export default function DemoChat() {
     }
 
     try {
-      const res = await fetch(`${BACKEND_URL}/chat`, {
+      const res = await fetch(`${BACKEND_URL}/webhook/demo-chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMsg.content, history: messages }),
